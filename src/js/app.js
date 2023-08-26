e88b5edb1e5e477a0b635ba9d5b011e24ccd298d
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-08-26 13:35:12 +0100

    prepare and style discover supPage

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,4 @@ new GreenAudioPlayer('.gap-example');
 new GreenAudioPlayer('.gap-example2');
 new GreenAudioPlayer('.gap-example3');
 new GreenAudioPlayer('.gap-example4');
+new GreenAudioPlayer('.gap-example5');
