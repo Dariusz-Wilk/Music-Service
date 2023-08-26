25e2a0ea821e3410b91e468818ea87d20baa2632
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-08-26 14:42:00 +0100

    implemented working navigation

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+/* eslint-disable indent */
+import { select, classNames } from './settings.js';
 import GreenAudioPlayer from '../vendor/green-audio-player.js';
 
 new GreenAudioPlayer('.gap-example');
@@ -5,3 +7,44 @@ new GreenAudioPlayer('.gap-example2');
 new GreenAudioPlayer('.gap-example3');
 new GreenAudioPlayer('.gap-example4');
 new GreenAudioPlayer('.gap-example5');
+
+const app = {
+	initPages: function () {
+		this.pages = document.querySelector(select.containerOf.pages).children;
+		this.navLinks = document.querySelectorAll(select.nav.navLinks);
+
+		const idFromHash = window.location.hash.replace('#/', '');
+		let pageMatchingHash = this.pages[0].id;
+
+		for (let page of this.pages) {
+			if (page.id == idFromHash) {
+				pageMatchingHash = page.id;
+				break;
+			}
+		}
+
+		this.activatePage(pageMatchingHash);
+
+		for (let link of this.navLinks) {
+			link.addEventListener('click', e => {
+				e.preventDefault();
+				const linkId = link.hash.replace('#', '');
+				this.activatePage(linkId);
+
+				window.location.hash = '#/' + linkId;
+			});
+		}
+	},
+
+	activatePage: function (pageId) {
+		for (let page of this.pages) {
+			page.classList.toggle(classNames.pages.active, page.id === pageId);
+		}
+	},
+	init: function () {
+		this.initPages();
+	},
+};
+
+app.init();
+console.log(app);
---
+++ src/js/settings.js
@@ -0,0 +1,18 @@
+/* eslint-disable indent */
+export const select = {
+	containerOf: {
+		pages: '.pages',
+	},
+	nav: {
+		navLinks: '.nav-link',
+	},
+};
+
+export const classNames = {
+	nav: {
+		active: 'active',
+	},
+	pages: {
+		active: 'active',
+	},
+};
